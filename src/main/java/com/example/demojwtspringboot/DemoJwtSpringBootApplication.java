58da1aebcaf64952a979bf695dda348088bbee06
NguyenQTinh <nguyentinhwork1998@gmail.com>
2022-04-22 09:28:17 +0700

    File Project

---
+++ src/main/java/com/example/demojwtspringboot/DemoJwtSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojwtspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJwtSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJwtSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demojwtspringboot/DemoJwtSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojwtspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
